773a30710e0854be52759987af80dc84a874da6b
Thomas Mapesa <mapesa@gmail.com>
2024-08-09 17:47:20 +0300

    moved delayMe to app/lib/data

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,16 +1,7 @@
-import { sql } from "@vercel/postgres";
+import { delayMe } from "@/app/lib/data";
 
 export default async function Page() {
     const num = await delayMe();
     console.log(num);
     return <p>Customers Page {num}</p>;
-  }
-
-  export async function delayMe() {
-
-    const customerCount = await sql`SELECT COUNT(*) FROM customers`;
-
-
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-    return Number(customerCount.rows[0] ?? '0');
   }
